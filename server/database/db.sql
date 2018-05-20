fd8cc81bd4862fdb189b07a0011e26de2c2d9621
jm18457 <jm18457@gmail.com>
2018-05-20 10:15:48 +0200

    Fix bugs

--- server/database/db.sql
+++ server/database/db.sql
@@ -1,24 +1,27 @@
-DROP DATABASE IF IT EXISTS fccVotingApp;
-CREATE DATABASE fccVotingApp;
+DROP DATABASE IF EXISTS fccvotingapp;
+CREATE DATABASE fccvotingapp;
 
-\c fccVotingApp;
+\c fccvotingapp;
 
 CREATE TABLE users (
-  id INTEGER SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   username varchar(80) UNIQUE,
   hash text,
   salt text
 );
 
 CREATE TABLE polls (
-  id INTEGER SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id) ON DELETE SET NULL,
   title varchar(80)
 );
 
 CREATE TABLE options (
-  id INTEGER SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   poll_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   name varchar(80),
   value INTEGER DEFAULT 0
 );
+
+INSERT INTO users (username, hash, salt) VALUES
+  ('admin', 'pass', 'salt');
