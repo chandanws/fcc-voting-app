38d600afe1fdc97c3d82a68da699e4d50b9b2157
jm18457 <jm18457@gmail.com>
2018-05-20 17:21:59 +0200

    fix HTTP method

--- server/api/polls/pollsRoute.js
+++ server/api/polls/pollsRoute.js
@@ -5,7 +5,7 @@ router.get("/", pollsController.polls_list);
 router.post("/", pollsController.polls_create);
 router.get("/:poll_id", pollsController.polls_detail);
 router.put("/:poll_id", pollsController.polls_update);
-router.put("/:poll_id", pollsController.polls_delete);
+router.delete("/:poll_id", pollsController.polls_delete);
 
 // TODO: bad URI to use /vote, check for better option, for now will do
 router.put("/:poll_id/vote", pollsController.polls_vote);
