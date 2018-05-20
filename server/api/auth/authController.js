31bf732a9fcb75349f852b524a8aa5eee8417a68
jm18457 <jm18457@gmail.com>
2018-05-20 19:56:37 +0200

    start creating registration test + route

--- server/api/auth/authController.js
+++ server/api/auth/authController.js
@@ -1,7 +1,36 @@
+const db = require("../../database").db;
+
 exports.login = (req, res) => {
   res.send("NOT IMPLEMENETED");
 };
 
 exports.register = (req, res) => {
   const { username, password } = req.body;
+  if (!password) {
+    res.status(400);
+    return res.send({
+      status: "fail",
+      data: { password: "password is missing" }
+    });
+  }
+
+  if (!username) {
+    res.status(400);
+    return res.send({
+      status: "fail",
+      data: { username: "username is missing" }
+    });
+  }
+
+  db
+    .result("SELECT * FROM users WHERE username = $1", [username])
+    .then(result => {
+      if (result.rowCount === 1) {
+        res.status(409);
+        return res.send("error");
+      }
+    })
+    .catch(err => {
+      return res.status(500).send("NOT IMPLEMENTED");
+    });
 };
---
+++ server/api/auth/authController.test.js
@@ -0,0 +1,61 @@
+const app = require("../../app");
+const request = require("supertest");
+const db = require("../../database").db;
+
+describe("register", () => {
+  beforeEach(() => {
+    return db.none("TRUNCATE users RESTART IDENTITY CASCADE");
+  });
+
+  // it("should create new user", () => {
+  //   return request(app)
+  //     .post("/auth/login")
+  //     .send({ username: "user1", password: "pass" })
+  //     .then(res => {
+  //       expect(res.statusCode).toBe(204);
+  //     });
+  // });
+
+  it("should not create user when password is missing", () => {
+    return request(app)
+      .post("/auth/register")
+      .send({ username: "user1" })
+      .then(res => {
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toHaveProperty("status", "fail");
+        expect(res.body).toHaveProperty("data", {
+          password: "password is missing"
+        });
+      });
+  });
+
+  it("should not create user when username is missing", () => {
+    return request(app)
+      .post("/auth/register")
+      .send({ password: "pass" })
+      .then(res => {
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toHaveProperty("status", "fail");
+        expect(res.body).toHaveProperty("data", {
+          username: "username is missing"
+        });
+      });
+  });
+
+  it("should not create user when username already taken", () => {
+    return db
+      .any("INSERT INTO users (username, hash, salt) VALUES ($1, $2, $3)", [
+        "user1",
+        "pass",
+        "salt"
+      ])
+      .then(() => {
+        return request(app)
+          .post("/auth/register")
+          .send({ username: "user1", password: "pass" })
+          .then(res => {
+            expect(res.statusCode).toBe(409);
+          });
+      });
+  });
+});
