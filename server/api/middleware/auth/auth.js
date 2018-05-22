3a1bfc1571b907a1bac3837c611a106d584bbbc9
jm18457 <jm18457@gmail.com>
2018-05-22 09:15:25 +0200

    remove log

--- server/api/middleware/auth/auth.js
+++ server/api/middleware/auth/auth.js
@@ -2,13 +2,13 @@ const jwt = require("jsonwebtoken");
 
 exports.authenticationRequired = (req, res, next) => {
   const authorization = req.get("Authorization");
+
   if (authorization && authorization.split(" ")[0] === "Bearer") {
     const token = authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
     // check expiration
     if (decoded.exp - Math.floor(Date.now() / 1000) <= 0) {
       res.status(401);
-      console.log("test");
       return res.json({
         status: "fail",
         data: { authorization: "Missing or Invalid Token" }
