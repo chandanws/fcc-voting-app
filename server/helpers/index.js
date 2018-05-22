82173c0326f014a992c5dd7b20b556e95248207d
jm18457 <jm18457@gmail.com>
2018-05-22 06:33:24 +0200

    Add create salt method

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,5 +1,14 @@
+const crypto = require("crypto");
+
 exports.decodeJWT = token => {
   const base64Url = token.split(".")[1];
   const base64 = base64Url.replace("-", "+").replace("-", "/");
   return JSON.parse(window.atob(base64));
+};
+
+exports.generateSalt = length => {
+  return crypto
+    .randomBytes(Math.ceil(length / 2))
+    .toString("hex")
+    .slice(0, length);
 };
--- server/helpers/index.test.js
+++ server/helpers/index.test.js
@@ -1,10 +1,24 @@
 const helpers = require("./index");
 
-describe("helpers.decodeJWT", () => {
-  it("should return object payload", () => {
-    const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJuYW1lIjoidXNlcjEiLCJpZCI6Mn0sImV4cCI6MTUyNjkyNDc3NiwiaWF0IjoxNTI2OTIxMTc2fQ.F9wdQEL3rlNxNQhXDOYGH2508CtvU-Xa-ZznobX5368";
-    const data = helpers.decodeJWT(token).data;
-    expect(data).toEqual({ id: 2, username: "user1" });
+describe("helpers", () => {
+  describe("decodeJWT", () => {
+    it("should return object payload", () => {
+      const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJuYW1lIjoidXNlcjEiLCJpZCI6Mn0sImV4cCI6MTUyNjkyNDc3NiwiaWF0IjoxNTI2OTIxMTc2fQ.F9wdQEL3rlNxNQhXDOYGH2508CtvU-Xa-ZznobX5368";
+      const data = helpers.decodeJWT(token).data;
+      expect(data).toEqual({ id: 2, username: "user1" });
+    });
+  });
+
+  describe("generateSalt", () => {
+    it("should return a length 16 string when supplied with value 16", () => {
+      const string = helpers.generateSalt(16);
+      expect(string.length).toBe(16);
+    });
+    it("should return different strings from different calls", () => {
+      const string1 = helpers.generateSalt(16);
+      const string2 = helpers.generateSalt(16);
+      expect(string1).not.toBe(string2);
+    });
   });
 });
