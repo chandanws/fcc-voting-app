53da4e2ab9a000d2ff2b4857096ec174e21de2b4
jm18457 <jm18457@gmail.com>
2018-05-22 09:02:06 +0200

    add auth route to deleting posts

--- server/api/polls/pollsRoute.js
+++ server/api/polls/pollsRoute.js
@@ -6,7 +6,11 @@ router.get("/", pollsController.polls_list);
 router.post("/", auth.authenticationRequired, pollsController.polls_create);
 router.get("/:poll_id", pollsController.polls_detail);
 router.put("/:poll_id", pollsController.polls_update);
-router.delete("/:poll_id", pollsController.polls_delete);
+router.delete(
+  "/:poll_id",
+  auth.authenticationRequired,
+  pollsController.polls_delete
+);
 
 // TODO: bad URI to use /vote, check for better option, for now will do
 router.put("/:poll_id/vote", pollsController.polls_vote);
