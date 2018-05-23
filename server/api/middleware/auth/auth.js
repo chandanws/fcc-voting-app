1429c7d26b17977b283afeb2dca46bfb92c13f7b
jm18457 <jm18457@gmail.com>
2018-05-23 18:17:30 +0200

    Fix authentication with expired token

--- server/api/middleware/auth/auth.js
+++ server/api/middleware/auth/auth.js
@@ -5,15 +5,17 @@ exports.authenticationRequired = (req, res, next) => {
 
   if (authorization && authorization.split(" ")[0] === "Bearer") {
     const token = authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-    // check expiration
-    if (decoded.exp - Math.floor(Date.now() / 1000) <= 0) {
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    } catch (err) {
       res.status(401);
       return res.json({
         status: "fail",
         data: { authorization: "Missing or Invalid Token" }
       });
     }
+
     res.locals.id = decoded.data.id;
     return next();
   }
