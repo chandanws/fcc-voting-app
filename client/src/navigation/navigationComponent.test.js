f1424ba756f7447510b3550a595d8802beef0570
jm18457 <jm18457@gmail.com>
2018-05-29 14:58:45 +0200

    Write test to check for DrawerComponent

--- client/src/navigation/navigationComponent.test.js
+++ client/src/navigation/navigationComponent.test.js
@@ -2,10 +2,19 @@ import React from "react";
 import { shallow } from "enzyme";
 import Navigation from "./navigationComponent";
 import Appbar from "./appbar/Appbar";
+import DrawerComponent from "./drawer/DrawerComponent";
 
 describe("<Navigation />", () => {
+  let wrapper;
+  beforeAll(() => {
+    wrapper = shallow(<Navigation />);
+  });
+
   it("should have one <Appbar />", () => {
-    const wrapper = shallow(<Navigation />);
     expect(wrapper.find(Appbar).length).toBe(1);
+  });
+
+  it("should have one <DrawerComponent />", () => {
+    expect(wrapper.find(DrawerComponent).length).toBe(1);
   });
 });
