33667276e9e821a14ff29350f8ee247b5b867b19
jm18457 <jm18457@gmail.com>
2018-05-29 11:23:56 +0200

    Add App component to be rendered

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
+import App from "./app/app";
 
-ReactDOM.render(<div>Hi</div>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
