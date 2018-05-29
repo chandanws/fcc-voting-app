aac9a3708f084c7990d56748a23fa834acb0059c
jm18457 <jm18457@gmail.com>
2018-05-29 15:09:32 +0200

    Add basic styling for navigation

--- client/src/navigation/navigationComponent.js
+++ client/src/navigation/navigationComponent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Appbar from "./appbar/Appbar";
 import DrawerComponent from "./drawer/DrawerComponent";
+import "./NavigationComponent.css";
 
 const Navigation = props => {
   return (
-    <div>
+    <div className="navigation">
       <Appbar />
       <DrawerComponent />
     </div>
