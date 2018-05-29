5be8d27f5e7a217473b130f4df47919d5dc65245
jm18457 <jm18457@gmail.com>
2018-05-29 14:58:36 +0200

    Add DrawerComponent to Navigation

--- client/src/navigation/navigationComponent.js
+++ client/src/navigation/navigationComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Appbar from "./appbar/Appbar";
+import DrawerComponent from "./drawer/DrawerComponent";
 
 const Navigation = props => {
   return (
     <div>
       <Appbar />
+      <DrawerComponent />
     </div>
   );
 };
