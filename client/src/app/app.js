65a6fb598f5932364d71a326143325cb44221851
jm18457 <jm18457@gmail.com>
2018-05-30 18:27:09 +0200

    Add PollComponent route

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -4,6 +4,7 @@ import HomepageComponent from "../pages/homepage/homepageComponent";
 import LoginComponent from "../pages/login/loginComponent";
 import RegisterComponent from "../pages/register/registerComponent";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import PollComponent from "../pages/poll/PollComponent";
 
 const App = props => {
   return (
@@ -21,6 +22,7 @@ const App = props => {
             <Route exact path="/" component={HomepageComponent} />
             <Route path="/login" component={LoginComponent} />
             <Route path="/register" component={RegisterComponent} />
+            <Route path="/polls/:id" component={PollComponent} />
           </main>
         </div>
       </Router>
