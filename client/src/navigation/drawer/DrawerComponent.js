35a5cec9224b7661519a1f8672ca36e04342491d
jm18457 <jm18457@gmail.com>
2018-05-30 07:14:36 +0200

    Create not drawer div, which can also be used to close drawer

--- client/src/navigation/drawer/DrawerComponent.js
+++ client/src/navigation/drawer/DrawerComponent.js
@@ -3,8 +3,15 @@ import "./DrawerComponent.css";
 import { useModifierWithBlock } from "../../helpers/helpers";
 const DrawerComponent = props => {
   const modifiers = props.drawerOpen ? [] : ["hidden"];
-  const classes = useModifierWithBlock("drawer", modifiers);
-  return <div className={classes}>Drawer Component</div>;
+  const classesDrawer = useModifierWithBlock("drawer", modifiers);
+  const classesNotDrawer = useModifierWithBlock("not-drawer", modifiers);
+  console.log(classesNotDrawer);
+  return (
+    <div className="drawer-wrapper">
+      <div className={classesDrawer}>Drawer Component</div>
+      <div onClick={props.toggleDrawer} className={classesNotDrawer} />
+    </div>
+  );
 };
 
 export default DrawerComponent;
