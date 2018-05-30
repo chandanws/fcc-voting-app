9122e8586337ea79ef902bfa103e9f83345c68a2
jm18457 <jm18457@gmail.com>
2018-05-30 09:31:33 +0200

    Add Router and login page

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Navigation from "../navigation";
+import HomepageComponent from "../pages/homepage/homepageComponent";
+import LoginComponent from "../pages/login/loginComponent";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const App = props => {
   return (
     <div>
-      <Navigation />
+      <Router>
+        <div>
+          <Navigation />
+          <main
+            style={{
+              position: "relative",
+              width: "90vw",
+              margin: "auto"
+            }}
+          >
+            <Route exact path="/" component={HomepageComponent} />
+            <Route path="/login" component={LoginComponent} />
+          </main>
+        </div>
+      </Router>
     </div>
   );
 };
