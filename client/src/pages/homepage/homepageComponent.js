8adc87f79ca60d664057167351f448fab5dbd4a7
jm18457 <jm18457@gmail.com>
2018-05-30 18:32:22 +0200

    Change ListItems so they are Links and link to dummy poll component

--- client/src/pages/homepage/homepageComponent.js
+++ client/src/pages/homepage/homepageComponent.js
@@ -2,30 +2,28 @@ import React from "react";
 import { Typography } from "../../Typography/Typography";
 import List, { ListItem } from "../../list/List";
 import { Button } from "../../button/ButtonComponent";
+import { Link } from "react-router-dom";
 
 const HomepageComponent = props => {
   return (
     <div>
       <Typography component="h1" text="All polls" />
-      <List>
+      <List modifiers={["bordered"]}>
         <ListItem>
-          <Button modifiers={["align-left"]}>Test</Button>
+          <Button component={Link} to="/polls/1" modifiers={["align-left"]}>
+            Test
+          </Button>
         </ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
-        <ListItem>Test</ListItem>
+        <ListItem>
+          <Button component={Link} to="/polls/2" modifiers={["align-left"]}>
+            Test
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button component={Link} to="/polls/3" modifiers={["align-left"]}>
+            Test
+          </Button>
+        </ListItem>
       </List>
     </div>
   );
