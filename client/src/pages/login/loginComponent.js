1e38efd17632416403dbc06730a468efc440f554
jm18457 <jm18457@gmail.com>
2018-05-30 13:38:54 +0200

    Convert loginComponent to class
    LoginComponent needs to handle the state for username, and password

--- client/src/pages/login/loginComponent.js
+++ client/src/pages/login/loginComponent.js
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import { Typography } from "../../Typography/Typography";
 import { Button } from "../../button/ButtonComponent";
-import "./loginComponent.css";
+import { FormElement, Form } from "../../form/FormComponent";
 
-const LoginComponent = props => {
-  return (
-    <div>
-      <Typography component="h1" text="Login" />
-      <form className="form">
-        <div className="form__wrapper">
-          <label className="form__label">Username</label>
-          <input
-            className="form__input"
-            type="text"
-            placeholder="randomuser1"
+class LoginComponent extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // do something with redux later
+    console.log(this.state);
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <Typography component="h1" text="Login" />
+        <Form onSubmit={this.handleSubmit}>
+          <FormElement
+            onChange={this.handleChange}
+            value={this.state.username}
+            label="username"
           />
-        </div>
-        <div className="form__wrapper">
-          <label className="form__label">Password</label>
-          <input className="form__input" type="password" />
-        </div>
-        <input className="form__submit" type="submit" value="Login" />
-      </form>
-    </div>
-  );
-};
+          <FormElement
+            onChange={this.handleChange}
+            value={this.state.password}
+            label="password"
+            type="password"
+          />
+          <input className="form__submit" type="submit" value="submit" />
+        </Form>
+      </div>
+    );
+  }
+}
 
 export default LoginComponent;
