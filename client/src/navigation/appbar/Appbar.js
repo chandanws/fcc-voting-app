ab726a9554429e21fb6bb9dfb8e421bac41946b6
jm18457 <jm18457@gmail.com>
2018-05-30 06:24:32 +0200

    Import ButtonComponent to use it

--- client/src/navigation/appbar/Appbar.js
+++ client/src/navigation/appbar/Appbar.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Button } from "../../button/ButtonComponent";
+import { Link } from "react-router-dom";
+import "./Appbar.css";
 
 const Appbar = props => {
   return (
-    <div>
-      <button>Menu</button>
-      <a href="/">Home</a>
+    <div className="appbar">
+      <Button className="appbar__button">Menu</Button>
+      {/* Later add component Link to button */}
+      <Button className="appbar__button">Home</Button>
     </div>
   );
 };
