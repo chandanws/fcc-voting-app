d7d7c19f1e6c39abc07f25f969ea12b3b05cc51c
jm18457 <jm18457@gmail.com>
2018-05-30 13:37:46 +0200

    Remove all non props from Form Component
    Previously I tried to make Form handle FormElement state,
    but hit a problem with with submitting, then I realized it would be easier to
    just  make state handled by the component that uses FormComponent, such
    as login

--- client/src/form/FormComponent.js
+++ client/src/form/FormComponent.js
@@ -1,80 +1,40 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import "./FormComponent.css";
 
 const FormContext = React.createContext([]);
 
-export class Form extends Component {
-  constructor(props) {
-    super(props);
+export const Form = ({ children, ...props }) => {
+  return (
+    <form className="form" {...props}>
+      {children}
+    </form>
+  );
+};
 
-    let numberOfInputs = React.Children.count(this.props.children) - 1; // last one is submit button
-    const values = this.createState(numberOfInputs);
-    this.state = { values };
-  }
+Form.propTypes = {
+  onSubmit: PropTypes.func.isRequired
+};
 
-  createState = value => {
-    let values = [];
+export const FormElement = ({ label, type, onChange, value }) => {
+  return (
+    <div className="form__wrapper">
+      <label className="form__label">{label}</label>
+      <input
+        type={type}
+        className="form__input"
+        value={value}
+        onChange={onChange(label)}
+      />
+    </div>
+  );
+};
 
-    let numberOfInputs = React.Children.count(this.props.children) - 1; // last one is submit button
-    for (let i = 0; i < numberOfInputs; i++) {
-      values = values.concat({ id: i, value: "" });
-    }
-    return values;
-  };
+FormElement.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string
+};
 
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log(this.state.values);
-    // this.props.handleSubmit();
-  };
-
-  // this needs to be passed down to each input
-  handleFormChange = index => event => {
-    console.log(index);
-    const newValues = this.state.values.map((value, _index) => {
-      if (index !== _index) {
-        return value;
-      } else {
-        return { ...value, value: event.target.value };
-      }
-    });
-    this.setState({
-      values: newValues
-    });
-  };
-
-  render() {
-    let max = React.Children.count(this.props.children) - 1;
-    let counter = 0;
-    const childs = React.Children.map(this.props.children, child => {
-      counter += 1;
-      return React.cloneElement(child, {
-        obj: {
-          data: this.state.values[counter - 1],
-          handleformchange: this.handleFormChange
-        }
-      });
-    });
-    return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        {childs}
-      </form>
-    );
-  }
-}
-
-export class FormElement extends Component {
-  render() {
-    const { label } = this.props;
-    return (
-      <div className="form__wrapper">
-        <label className="form__label">{label}</label>
-        <input
-          className="form__input"
-          value={this.props.obj.data.value}
-          onChange={this.props.obj.handleformchange(this.props.obj.data.id)}
-        />
-      </div>
-    );
-  }
-}
+FormElement.defaultProps = {
+  type: "text"
+};
