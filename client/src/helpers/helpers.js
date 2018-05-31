c796723fd18cce2d9f04126dff518ab49fc50aad
jm18457 <jm18457@gmail.com>
2018-05-31 06:29:33 +0200

    Create function to toggleElement

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -11,3 +11,20 @@ export const useModifierWithBlock = (block, modifiers = []) => {
   );
   return className;
 };
+
+/**
+ * Checks the passed array for the passed element.
+ * If element is found it removes it.
+ * If element is not found it adds it to the end of the array.
+ * @param {Array} array simple array
+ * @param {any} element any value that could be in the passed array
+ * @return {Array} passed array with either removed or added element
+ */
+export const toggleElementInArray = (array, element) => {
+  const index = array.indexOf(element);
+  if (index === -1) {
+    return [...array, element];
+  }
+  const newArray = array.slice(0, index).concat(array.slice(index + 1));
+  return newArray;
+};
--- client/src/helpers/helpers.test.js
+++ client/src/helpers/helpers.test.js
@@ -1,4 +1,4 @@
-import { useModifierWithBlock } from "./helpers";
+import { useModifierWithBlock, toggleElementInArray } from "./helpers";
 
 describe("useModifierWithBlock", () => {
   it("should return button, when no modifier specified", () => {
@@ -9,5 +9,28 @@ describe("useModifierWithBlock", () => {
     expect(useModifierWithBlock("button", ["stroked", "ugly"])).toBe(
       "button button--stroked button--ugly"
     );
+  });
+});
+
+describe("toggleElementInArray", () => {
+  let oldArray;
+  beforeAll(() => {
+    oldArray = [1, 3, 5, 7];
+  });
+  it("should return added array if no element found", () => {
+    expect(toggleElementInArray(oldArray, 2)).toEqual([...oldArray, 2]);
+  });
+
+  it("should return array with 1 value if initial array is empty", () => {
+    const emptyArray = [];
+    expect(toggleElementInArray(emptyArray, 1)).toEqual([...emptyArray, 1]);
+  });
+
+  it("should return proper array when index at 0", () => {
+    expect(toggleElementInArray(oldArray, 1)).toEqual([3, 5, 7]);
+  });
+
+  it("should return proper array when index at 0", () => {
+    expect(toggleElementInArray(oldArray, 7)).toEqual([1, 3, 5]);
   });
 });
