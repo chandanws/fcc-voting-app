9c2092a8e3eff52cc1cbe062b886a62a326072d4
jm18457 <jm18457@gmail.com>
2018-05-31 08:38:19 +0200

    Add route to create new poll

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -3,7 +3,9 @@ import Navigation from "../navigation";
 import HomepageComponent from "../pages/homepage/homepageComponent";
 import LoginComponent from "../pages/login/loginComponent";
 import RegisterComponent from "../pages/register/registerComponent";
+import CreatePollComponent from "../pages/createpoll/createpollComponent";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import PollComponent from "../pages/poll/PollComponent";
 
 const App = props => {
@@ -22,7 +24,10 @@ const App = props => {
             <Route exact path="/" component={HomepageComponent} />
             <Route path="/login" component={LoginComponent} />
             <Route path="/register" component={RegisterComponent} />
-            <Route path="/polls/:id" component={PollComponent} />
+            <Switch>
+              <Route path="/polls/new" component={CreatePollComponent} />
+              <Route path="/polls/:id" component={PollComponent} />
+            </Switch>
           </main>
         </div>
       </Router>
