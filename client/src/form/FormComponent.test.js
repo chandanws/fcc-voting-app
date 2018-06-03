0c2b281c90a499c1ec524d4c830717f5f5a40e40
jm18457 <jm18457@gmail.com>
2018-06-03 08:21:13 +0200

    Remove unnecessary import

--- client/src/form/FormComponent.test.js
+++ client/src/form/FormComponent.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Form, FormElement } from "./FormComponent";
 import { shallow } from "enzyme";
-import toJson from "enzyme-to-json";
 
 describe("<Form />", () => {
   it("should render correctly", () => {
