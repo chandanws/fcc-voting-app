215a17acaa0761f15a3ed844473d923b6eba7544
jm18457 <jm18457@gmail.com>
2018-06-03 09:27:12 +0200

    change import name and remove wrong child

--- client/src/list/ListComponent.test.js
+++ client/src/list/ListComponent.test.js
@@ -1,12 +1,11 @@
 import React from "react";
-import List, { ListItem } from "./List";
+import List, { ListItem } from "./ListComponent";
 import { shallow } from "enzyme";
 
 describe("<List />", () => {
   it("should make correct list items", () => {
     const wrapper = shallow(
       <List>
-        <List.Item>1</List.Item>
         <ListItem>2</ListItem>
         <ListItem>3</ListItem>
         <ListItem>4</ListItem>
