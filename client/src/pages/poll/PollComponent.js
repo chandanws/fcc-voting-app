4a2fa195e2e4c520d325c94c1057b170083cf44c
jm18457 <jm18457@gmail.com>
2018-06-03 06:53:26 +0200

    Use recharts instead of chartjs

--- client/src/pages/poll/PollComponent.js
+++ client/src/pages/poll/PollComponent.js
@@ -1,23 +1,29 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Typography } from "../../Typography/Typography";
-import { Pie } from "react-chartjs-2";
+import {
+  PieChart,
+  Pie,
+  Legend,
+  Sector,
+  Cell,
+  Label,
+  ResponsiveContainer
+} from "recharts";
 import { Form, FormElement } from "../../form/FormComponent";
 import CommentContainer from "../../comment/CommentContainer";
+import "./PollComponent.css";
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 export default class PollComponent extends Component {
   state = {
     value: "something",
-    dummyData: {
-      labels: ["Green", "Blue", "Black"],
-      datasets: [
-        {
-          data: [300, 200, 50],
-          backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-          hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"]
-        }
-      ]
-    }
+    dummyData: [
+      { name: "B", value: 400 },
+      { name: "A", value: 200 },
+      { name: "C", value: 600 }
+    ]
   };
 
   handleChange = e => this.setState({ value: e.target.value });
@@ -28,29 +34,40 @@ export default class PollComponent extends Component {
   };
   render() {
     return (
-      <div>
+      <div style={{ width: "100%" }}>
         {/* TODO: Change text to be based on data from redux*/}
         <Typography component="h1" text="Best Actor" />
-        <Pie width={200} data={this.state.dummyData} />
-        <Form onSubmit={this.handleSubmit}>
-          <label className="form__label">
-            Select option:
-            <select
-              style={{
-                fontSize: "2rem",
-                marginLeft: "1rem",
-                border: "1px solid black"
-              }}
-              value={this.state.value}
-              onChange={this.handleChange}
-            >
-              <option value="something">Something</option>
-              <option value="user">User</option>
-              <option value="weird">Weird</option>
-              <option value="anti">Anti</option>
-            </select>
-          </label>
-        </Form>
+        <div className="chart-form-wrapper">
+          <ResponsiveContainer aspect={1}>
+            <PieChart style={{ fontSize: "2rem" }} width={800} height={400}>
+              <Legend verticalAlign="top" height={36} />
+              <Pie label fill="#8884d8" data={this.state.dummyData}>
+                {this.state.dummyData.map((entry, index) => (
+                  <Cell fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+          <Form className="chart__form" onSubmit={this.handleSubmit}>
+            <label className="form__label">
+              Select option:
+              <select
+                style={{
+                  fontSize: "2rem",
+                  marginLeft: "1rem",
+                  border: "1px solid black"
+                }}
+                value={this.state.value}
+                onChange={this.handleChange}
+              >
+                <option value="something">Something</option>
+                <option value="user">User</option>
+                <option value="weird">Weird</option>
+                <option value="anti">Anti</option>
+              </select>
+            </label>
+          </Form>
+        </div>
         <Typography component="h2" text="Comments" />
         {/* Margin left fix for first element */}
         <div style={{ marginLeft: "-2rem" }}>
