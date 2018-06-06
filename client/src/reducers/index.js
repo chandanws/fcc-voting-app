2ae7f1f9a021c86518416ed8f14d99ea11db8793
jm18457 <jm18457@gmail.com>
2018-06-06 21:42:16 +0200

    add comments reducer to combination

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import polls from "./pollsReducer";
 import specificPoll from "./specificPollReducer";
+import comments from "./commentReducer";
 
 export default combineReducers({
   polls,
-  specificPoll
+  specificPoll,
+  comments
 });
