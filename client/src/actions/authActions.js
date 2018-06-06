5345cd096646f063c25a45e7b07630fbf1309661
jm18457 <jm18457@gmail.com>
2018-06-06 22:07:41 +0200

    Add login actions

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,44 @@
+import { decodeJWT } from "../helpers/helpers";
+
+export const CHECK_TOKEN_LOGIN = "CHECK_TOKEN_LOGIN";
+const requestTokenLogin = login => {
+  return {
+    type: CHECK_TOKEN_LOGIN,
+    payload: login
+  };
+};
+
+export const REQUEST_LOGIN = "REQUEST_LOGIN";
+const requestLogin = () => ({
+  type: REQUEST_LOGIN
+});
+
+export const RECEIVE_LOGIN = "RECEIEVE_LOGIN";
+export const receiveLogin = userData => ({
+  type: RECEIVE_LOGIN,
+  payload: userData
+});
+
+export const fetchLogin = (username, password) => {
+  return dispatch => {
+    dispatch(requestLogin());
+    return fetch(`/auth/login`, {
+      body: JSON.stringify({ username, password }),
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, same-origin, *omit
+      headers: {
+        "user-agent": "Mozilla/4.0 MDN Example",
+        "content-type": "application/json"
+      },
+      method: "POST",
+      mode: "cors", // no-cors, cors, *same-origin
+      redirect: "follow", // manual, *follow, error
+      referrer: "no-referrer" // *client, no-referrer
+    }).then(response => {
+      const token = response.headers.get("authorization").split(" ")[1];
+      localStorage.setItem("fcc-voting-app-token", token);
+      const decoded = decodeJWT(token).data;
+      dispatch(receiveLogin(decoded));
+    });
+  };
+};
