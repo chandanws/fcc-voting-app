47da385310ed7b098391cedeebe21e3f4046baf8
jm18457 <jm18457@gmail.com>
2018-06-06 22:09:24 +0200

    Add login reducer
    Name problem right now, don't know how to handle
    registration right now with auth

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import polls from "./pollsReducer";
 import specificPoll from "./specificPollReducer";
 import comments from "./commentReducer";
+import login from "./authReducer";
 
 export default combineReducers({
   polls,
   specificPoll,
-  comments
+  comments,
+  login
 });
