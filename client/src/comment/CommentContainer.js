863d26a9af6efe63204ba51045028b89189d40d7
jm18457 <jm18457@gmail.com>
2018-06-06 21:41:30 +0200

    Use redux store comments state instead of dummy state

--- client/src/comment/CommentContainer.js
+++ client/src/comment/CommentContainer.js
@@ -5,80 +5,28 @@ import {
   toggleObjectsInArray,
   changeValueInArrayOfObjects
 } from "../helpers/helpers";
+import { connect } from "react-redux";
+import { fetchComments } from "../actions/commentActions";
 
-export default class CommentContainer extends Component {
+class CommentContainer extends Component {
   // comments will later be moved to redux state
   // closed comments and open reply boxes will move to new state
   // open replies => [ {id: 1, value: "something"}, {id: 2, value: "something" }]
   state = {
-    comments: [
-      {
-        id: 1,
-        username: "user1",
-        body: "this is random body",
-        children: []
-      },
-      {
-        id: 2,
-        username: "user2",
-        body: "this is random body",
-        children: [
-          {
-            id: 5,
-            username: "user3",
-            body: "this is random body",
-            children: [
-              {
-                id: 30,
-                username: "user1",
-                body: "this is random body",
-                children: []
-              },
-              {
-                id: 7,
-                username: "user1",
-                body: "this is random body",
-                children: []
-              }
-            ]
-          },
-          {
-            id: 4,
-            username: "user1",
-            body: "this is random body",
-            children: []
-          }
-        ]
-      },
-      {
-        id: 31,
-        username: "user1",
-        body: "this is random body",
-        children: [
-          {
-            id: 32,
-            username: "user1",
-            body: "this is random body",
-            children: []
-          }
-        ]
-      },
-      {
-        id: 33,
-        username: "user1",
-        body: "this is random body",
-        children: []
-      }
-    ],
     closed: [],
     openReplies: []
   };
+
+  componentDidMount() {
+    this.props.fetchComments(this.props.poll_id);
+  }
 
   toggleOpenReplies = id => {
     const newArray = toggleObjectsInArray(this.state.openReplies, id);
     this.setState({
       openReplies: newArray
     });
+    console.log("Test", newArray);
   };
 
   toggleTree = id => {
@@ -100,20 +48,36 @@ export default class CommentContainer extends Component {
   };
 
   render() {
+    console.log("Comments", this.props.comments);
     return (
       <div style={{ marginLeft: "-2rem" }}>
-        <Comments
-          openReplies={this.state.openReplies}
-          toggleOpenReplies={this.toggleOpenReplies}
-          handleReplyState={this.handleReplyState}
-          closed={this.state.closed}
-          toggleTree={this.toggleTree}
-          comments={this.state.comments}
-        />
+        {this.props.comments.data[0] && (
+          <Comments
+            openReplies={this.state.openReplies}
+            toggleOpenReplies={this.toggleOpenReplies}
+            handleReplyState={this.handleReplyState}
+            closed={this.state.closed}
+            toggleTree={this.toggleTree}
+            comments={this.props.comments.data}
+          />
+        )}
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  comments: state.comments
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchComments: poll_id => dispatch(fetchComments(poll_id))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CommentContainer);
 
 export const Comments = props => {
   return (
