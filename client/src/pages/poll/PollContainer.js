aa4151491ccafab4a7e3af6d31dc861848e68d5b
jm18457 <jm18457@gmail.com>
2018-06-06 18:11:50 +0200

    add vote method dispatch contianer

--- client/src/pages/poll/PollContainer.js
+++ client/src/pages/poll/PollContainer.js
@@ -1,14 +1,18 @@
 import PollComponent from "./PollComponent";
 import { connect } from "react-redux";
 import { fetchPolls } from "../../actions/pollsActions";
-import { fetchSpecificPoll } from "../../actions/specificPollActions";
+import {
+  fetchSpecificPoll,
+  initiatePollVote
+} from "../../actions/specificPollActions";
 
 const mapStateToProps = state => ({
   specificPoll: state.specificPoll
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchSpecificPoll: poll_id => dispatch(fetchSpecificPoll(poll_id))
+  fetchSpecificPoll: poll_id => dispatch(fetchSpecificPoll(poll_id)),
+  vote: (poll_id, option_id) => dispatch(initiatePollVote(poll_id, option_id))
 });
 
 export default connect(
