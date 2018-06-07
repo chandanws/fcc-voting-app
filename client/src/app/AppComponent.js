6325fd4b564707b0e6ec61b471103fea8b884a7e
jm18457 <jm18457@gmail.com>
2018-06-07 09:20:41 +0200

    convert to class

--- client/src/app/AppComponent.js
+++ client/src/app/AppComponent.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import Navigation from "../navigation";
 import HomepageContainer from "../pages/homepage/homepageContainer";
-import LoginContainer from "../pages/login/LoginContainer";
 import RegisterComponent from "../pages/register/registerComponent";
 import CreatePollComponent from "../pages/createpoll/createpollComponent";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -9,32 +8,38 @@ import { Switch } from "react-router-dom";
 import PollContainer from "../pages/poll/PollContainer";
 import "./AppComponent.css";
 import ProfileComponent from "../pages/profile/ProfileComponent";
-import { WithAuth } from "../middleware/authentication";
+import { WithAuth, WithoutLogIn } from "../middleware/authentication";
+import LoginContainer from "../pages/login/LoginContainer";
 
-const App = props => {
-  return (
-    <div>
-      <Router>
-        <div>
-          <Navigation />
-          <main className="main">
-            <Route exact path="/" component={HomepageContainer} />
-            <Route path="/login" component={LoginContainer} />
-            <Route path="/register" component={RegisterComponent} />
-            <WithAuth
-              path="/profile"
-              component={ProfileComponent}
-              isLogged={false}
-            />
-            <Switch>
-              <WithAuth path="/polls/new" component={CreatePollComponent} />
-              <Route path="/polls/:id" component={PollContainer} />
-            </Switch>
-          </main>
-        </div>
-      </Router>
-    </div>
-  );
-};
-
-export default App;
+export default class App extends Component {
+  render() {
+    const { loginState } = this.props;
+    return (
+      <div>
+        <Router>
+          <div>
+            <Navigation />
+            <main className="main">
+              <Route exact path="/" component={HomepageContainer} />
+              <Route path="/register" component={RegisterComponent} />
+              <WithAuth
+                path="/profile"
+                component={ProfileComponent}
+                isLogged={true}
+              />
+              <WithoutLogIn
+                path="/login"
+                component={LoginContainer}
+                isLogged={loginState.username !== ""}
+              />
+              <Switch>
+                <WithAuth path="/polls/new" component={CreatePollComponent} />
+                <Route path="/polls/:id" component={PollContainer} />
+              </Switch>
+            </main>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
