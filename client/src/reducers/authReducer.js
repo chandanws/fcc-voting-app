74b29e5dd9d6ea875e1278efae0ead8d23d79110
jm18457 <jm18457@gmail.com>
2018-06-07 08:48:50 +0200

    add case for login failure

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,13 +1,16 @@
 import {
   REQUEST_LOGIN,
   RECEIVE_LOGIN,
-  CHECK_TOKEN_LOGIN
+  CHECK_TOKEN_LOGIN,
+  FAIL_LOGIN
 } from "../actions/authActions";
 
 const initialState = {
   isChecking: false,
   id: null,
-  username: ""
+  username: "",
+  fail: false,
+  failMessage: ""
 };
 
 export default (state = initialState, action) => {
@@ -19,7 +22,17 @@ export default (state = initialState, action) => {
         ...state,
         isChecking: false,
         username: action.payload.username,
-        id: action.payload.id
+        id: action.payload.id,
+        fail: false
+      };
+    case FAIL_LOGIN:
+      return {
+        ...state,
+        isChecking: false,
+        username: "",
+        id: null,
+        fail: true,
+        failMessage: action.payload
       };
     default:
       return state;
