6930a185e05a1509101f9953b95ea7c4b1878bf2
jm18457 <jm18457@gmail.com>
2018-06-07 06:19:36 +0200

    Add wrapper to center loading circle

--- client/src/loading/LoadingCircle.js
+++ client/src/loading/LoadingCircle.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./LoadingCircle.css";
+
+export const LoadingWrapper = ({ children }) => {
+  return <div className="loading-circle-wrapper">{children}</div>;
+};
+
 export const LoadingCircle = () => {
   return <div className="loading-circle" />;
 };
