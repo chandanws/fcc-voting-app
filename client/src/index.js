5c2cd55726583b5256cb7a8b30e487b06be91c02
jm18457 <jm18457@gmail.com>
2018-06-07 09:20:01 +0200

    User AppContainer

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
-import App from "./app/AppComponent";
+import AppContainer from "./app/AppContainer";
 import configureStore from "./configureStore";
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AppContainer />
   </Provider>,
   document.getElementById("root")
 );
