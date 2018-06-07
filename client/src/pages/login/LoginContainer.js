db4821c8f163d29594d254651b2d55021486309b
jm18457 <jm18457@gmail.com>
2018-06-07 08:48:14 +0200

    add mapstatetoprops with loginstate

--- client/src/pages/login/LoginContainer.js
+++ client/src/pages/login/LoginContainer.js
@@ -2,11 +2,15 @@ import { connect } from "react-redux";
 import LoginComponent from "./loginComponent";
 import { fetchLogin } from "../../actions/authActions";
 
+const mapStateToProps = state => ({
+  loginState: state.login
+});
+
 const mapDispatchToProps = dispatch => ({
   login: (username, password) => dispatch(fetchLogin(username, password))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(LoginComponent);
